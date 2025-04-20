4b6b84ef7b2ea8bdacead98f78b2a6bd06895980
sharvari mehta <msharvari97@gmail.com>
2025-04-20 23:19:03 +0530

    initial commit

--- Java Practice Programs/src/b2_input_output/input_output_p.java
+++ Java Practice Programs/src/b2_input_output/input_output_p.java
@@ -6,22 +6,13 @@ package b2_input_output;
 	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
 
-	        // Take input for byte
-	        System.out.print("Enter a byte value: ");
-	        byte byteValue = scanner.nextByte();
-
-	        // Take input for short
-	        System.out.print("Enter a short value: ");
-	        short shortValue = scanner.nextShort();
+	      
 
 	        // Take input for int
 	        System.out.print("Enter an int value: ");
 	        int intValue = scanner.nextInt();
 
-	        // Take input for long
-	        System.out.print("Enter a long value: ");
-	        long longValue = scanner.nextLong();
-
+	       
 	        // Take input for float
 	        System.out.print("Enter a float value: ");
 	        float floatValue = scanner.nextFloat();
@@ -40,10 +31,9 @@ package b2_input_output;
 
 	        // Display the outputs
 	        System.out.println("\nYou entered:");
-	        System.out.println("byte: " + byteValue);
-	        System.out.println("short: " + shortValue);
+	     
 	        System.out.println("int: " + intValue);
-	        System.out.println("long: " + longValue);
+	        
 	        System.out.println("float: " + floatValue);
 	        System.out.println("double: " + doubleValue);
 	        System.out.println("char: " + charValue);
