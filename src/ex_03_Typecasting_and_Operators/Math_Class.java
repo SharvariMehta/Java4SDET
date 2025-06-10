8476ae3570d7380f35012fa55d31ba819b22483b
sharvari mehta <msharvari97@gmail.com>
2025-06-10 23:13:34 +0530

    updating Math Class Code

---
+++ src/ex_03_Typecasting_and_Operators/Math_Class.java
@@ -0,0 +1,46 @@
+
+
+package ex_03_Typecasting_and_Operators;
+import java.util.*;
+public class Math_Class {
+    public static void main(String[] args) {
+        double x = 25.0;
+        double y = 10.0;
+        int a = 20;
+        int b = 10;
+
+        // Addition:
+        System.out.println("Sum of a and b : " + Math.addExact(a, b));
+
+        // Square root
+        System.out.println("√" + x + " = " + Math.sqrt(x));
+
+        // Power
+        System.out.println(x + "^" + y + " = " + Math.pow(x, y));
+
+        // Maximum
+        System.out.println("Max of " + x + " and " + y + " = " + Math.max(x, y));
+
+        // Minimum
+        System.out.println("Min of " + x + " and " + y + " = " + Math.min(x, y));
+
+        // Absolute value
+        System.out.println("Absolute of -y = " + Math.abs(-y));
+
+        // Random number between 0.0 and 1.0
+        System.out.println("Random number = " + Math.random());
+    }
+
+}
+
+/*
+OUTPUT:
+Sum of a and b : 30
+√25.0 = 5.0
+25.0^10.0 = 9.5367431640625E13
+Max of 25.0 and 10.0 = 25.0
+Min of 25.0 and 10.0 = 10.0
+Absolute of -y = 10.0
+Random number = 0.0721208142080475
+ */
+
---
+++ src/ex_25_java_codes_1_Normal/Operators_Concatenation.java
@@ -0,0 +1,15 @@
+package ex_25_java_codes_1_Normal;
+
+public class Operators_Concatenation {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        String FirstName = "Sharvari";
+        String Lastname = "Mehta";
+        System.out.println(a + b + FirstName + Lastname);   // Performs Addition Function
+        System.out.println(FirstName + Lastname + a + b);   // Doesn't perform Addition Function
+        System.out.println(FirstName + Lastname + (a + b)); // Performs Addition Function
+
+    }
+
+}
