885ee076ebd083b5aa7bfe690755fbe914832e53
sharvari mehta <msharvari97@gmail.com>
2025-06-15 19:47:39 +0530

    updating InputUsingConsole Code

---
+++ src/ex_01_Java_Basics/InputUsingConsole.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class InputUsingConsole {
+    public static void main(String[] args) {
+        String ip = args[0];
+// Navigate to ^ symbol above --> Edit Configuration --> Give Input in Program Arguments Field
+        System.out.println("Input taken by Console : "+ip);
+    }
+}
+/*
+OUTPUT:
+Input taken by Console : 65
+ */
