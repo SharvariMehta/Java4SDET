a5134eb84074de2f26383b9de48eedf02ff6e2ff
sharvari mehta <msharvari97@gmail.com>
2025-07-03 00:05:51 +0530

    updating Control Flow Code

---
+++ src/ex_04_Control_Flow/For_Loop.java
@@ -0,0 +1,14 @@
+package ex_04_Control_Flow;
+
+public class For_Loop {
+    public static void main(String[] args) {
+        for(int i = 0; i< 5; i++)
+        {
+            System.out.println(i);
+        }
+        for(int j = 0; j< 5; ++j)
+        {
+            System.out.println(j);
+        }
+    }
+}
