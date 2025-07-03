b172c1faab4ca3ac964a30011fabe91bc6922c3d
sharvari mehta <msharvari97@gmail.com>
2025-07-03 18:30:40 +0530

    updating Control Flow Code

---
+++ src/ex_04_Control_Flow/Switch_Case_Arrows.java
@@ -0,0 +1,17 @@
+package ex_04_Control_Flow;
+
+public class Switch_Case_Arrows {
+    public static void main(String[] args) {
+        int n = 001;
+        switch(n)
+        {
+            case 001 -> System.out.println("001");
+            case 002 -> System.out.println("001");
+            case 003,004 -> System.out.println("001");
+        }
+    }
+}
+/*
+OUTPUT:
+001
+ */
