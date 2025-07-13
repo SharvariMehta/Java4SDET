34203bc6cd046e07a0ba0911982b4ce0d14085e6
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-14 01:44:13 +0530

    Updating vowel and consonants Code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

---
+++ src/ex_20_Java_Codes_1_Normal/Count_Vowels_Consonants_Using_Function.java
@@ -0,0 +1,45 @@
+package ex_20_Java_Codes_1_Normal;
+
+import java.util.Scanner;
+
+public class Count_Vowels_Consonants_Using_Function {
+    public static void main(String[] args) {
+        System.out.println("Enter a String to count Vowels : ");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        System.out.println("Number of vowels in String are : "+vowelcount(userInput));
+        System.out.println("Number of Consonants in String are : "+consonantcount(userInput));
+    }
+
+    public static int vowelcount(String userInput)
+    {
+        int v = 0;
+        for(int i = 0; i<=userInput.length()-1; i++)
+        {
+            if(userInput.charAt(i) == 'a' || userInput.charAt(i) == 'e' || userInput.charAt(i) == 'i' || userInput.charAt(i) == 'o' || userInput.charAt(i) == 'u')
+            {
+                v++;
+            }
+        }
+        return v;
+    }
+
+    public static int consonantcount(String userInput)
+    {
+        int c = 0;
+        for(int i = 0; i<=userInput.length()-1; i++)
+        {
+            if(userInput.charAt(i) != 'a' && userInput.charAt(i) != 'e' && userInput.charAt(i) != 'i' && userInput.charAt(i) != 'o' && userInput.charAt(i) != 'u')
+            {
+                c++;
+            }
+        }
+        return c;
+    }
+}
+/*
+OUTPUT:
+SharvariMehta
+Number of vowels in String are : 5
+Number of Consonants in String are : 8
+*/
