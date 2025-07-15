8c1532c16145b6258a21ad9095565e26898a7ef1
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-15 23:38:50 +0530

    Updating Arrays Code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

---
+++ src/ex_05_Arrays/Array_Declaration.java
@@ -0,0 +1,13 @@
+package ex_05_Arrays;
+
+public class Array_Declaration {
+    public static void main(String[] args) {
+        //Static Allocation:
+        int[] marks = {10,40,20,30,70};
+        System.out.println(marks[0]);
+
+
+        //Dynamic Allocation:
+
+    }
+}
