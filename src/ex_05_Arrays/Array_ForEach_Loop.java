082dc50a6d87fb1e0b7c642b0a781aec9f39f989
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-16 21:55:36 +0530

    Updating Arrays Code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

--- src/ex_05_Arrays/Array_ForEach_Loop.java
+++ src/ex_05_Arrays/Array_ForEach_Loop.java
@@ -2,6 +2,25 @@ package ex_05_Arrays;
 
 public class Array_ForEach_Loop {
     public static void main(String[] args) {
+        int[] n = {1,2,3,4,5,6,7,8,9};
+        System.out.print("Printing an integer array using for-each Loop : ");
+        for(int i : n)
+        {
+            System.out.print(i+" ");
+        }
 
+        System.out.println();
+
+        String[] fruits = {"apples", "mangoes", "oranges"};
+        System.out.print("Printing a String Array using For-each Loop : ");
+        for(String i : fruits)
+        {
+            System.out.print(i+" ");
+        }
     }
 }
+/*
+OUTPUT:
+Printing an integer array using for-each Loop : 1 2 3 4 5 6 7 8 9
+Printing a String Array using For-each Loop : apples mangoes oranges
+ */
--- src/ex_05_Arrays/Array_Max_Min.java
+++ src/ex_05_Arrays/Array_Max_Min.java
@@ -2,40 +2,38 @@ package ex_05_Arrays;
 
 public class Array_Max_Min {
     public static void main(String[] args) {
-        int[] n = {10, 20, 90, 40, 50, 80};
 
-        int maxnum = maximum(n);
-        System.out.println("Maximum Number in Array n is : " + maxnum);
+        int[] n = {1, 9, 2, 8, 3, 7, 4, 6, 5, 0};
 
-        int minnum = minimum(n);
-        System.out.println("Minimum Number in Array n is : "+minnum);
+        int maxnum = maximumfunction(n);
+        System.out.println("Maximum Number in Array is : "+maxnum);
+
+        int minnum = minimumfunction(n);
+        System.out.println("Minimum Number in Array is : "+minnum);
     }
 
-    public static int maximum(int[] n) {
-        int max = n[0];
-        for (int i = 0; i < n.length; i++)
-        {
-            if(max<n[i])
-                max = n[i];
-        }
-        return max;
-    }
-
-    public static int minimum(int[] n)
-    {
+    private static int minimumfunction(int[] n) {
         int min = n[0];
         for(int i=0; i<n.length; i++)
         {
             if(min>n[i])
-            {
                 min = n[i];
-            }
         }
         return min;
+    }
+
+    private static int maximumfunction(int[] n) {
+        int max = n[0];
+        for(int i=0; i<n.length; i++)
+        {
+            if(max<=n[i])
+                max = n[i];
+        }
+        return max;
     }
 }
 /*
 OUTPUT:
-Maximum Number in Array n is : 90
-Minimum Number in Array n is : 10
+Maximum Number in Array is : 9
+Minimum Number in Array is : 0
  */
