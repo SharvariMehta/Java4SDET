a234bdf9c288b44cbb76dc0fc0562ab94f5bcd17
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-16 08:30:32 +0530

    Updating Arrays Code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

--- src/ex_05_Arrays/Array_Max_Min.java
+++ src/ex_05_Arrays/Array_Max_Min.java
@@ -4,10 +4,9 @@ public class Array_Max_Min {
     public static void main(String[] args) {
         int[] n = {10,20,90,40,50,80};
 
-        int max = 0;
+        int max = n[0];
         for(int i = 0; i<n.length; i++)
         {
-            max = n[i];
             if(max<n[i])
             {
                 max = n[i];
@@ -15,14 +14,13 @@ public class Array_Max_Min {
         }
         System.out.println("Maximum Number in Array n is : "+max);
 
-        int min = 0;
+        int min = n[0];
         for(int i = 0; i<n.length; i++)
         {
             if(min>=n[i])
             {
                 min = n[i];
             }
-            //min = n[i];
         }
         System.out.println("Minimum Number in Array n is : "+min);
     }
