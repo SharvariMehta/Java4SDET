a2bdc7b698fbe32ba760f6bb0f95e6ba5073374f
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-17 17:10:49 +0530

    Updating Arrays Code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

---
+++ src/ex_05_Arrays/Array_2D.java
@@ -0,0 +1,16 @@
+package ex_05_Arrays;
+
+import java.util.Arrays;
+
+public class Array_2D {
+    public static void main(String[] args) {
+
+        int[][] Array_2D = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        System.out.println(Array_2D);
+    }
+
+}
---
+++ src/ex_05_Arrays/Array_Jagged.java
@@ -0,0 +1,9 @@
+package ex_05_Arrays;
+
+public class Array_Jagged {
+    int[][] Jagged_Array ={
+            {1,2,3},
+            {1,2,3,4,5},
+            {1,2}
+    };
+}
