93c21eaa7cd998cf7a06d1f806efbedc5750477f
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-22 00:28:52 +0530

    Updating Access Modifier Code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

---
+++ src/ex_12_Access_Modifiers_And_Keywords/AM_Default.java
@@ -0,0 +1,34 @@
+package ex_12_Access_Modifiers_And_Keywords;
+//Accessed in same class, another class and same package
+//NOT accessed in child class
+public class AM_Default {
+    int sal = 7;
+    void sameClass() {
+        System.out.println("Variable is accessed in same Class : "+sal);
+    }
+    public static void main(String[] args) {
+        AM_Default d1 = new AM_Default();
+        d1.sameClass();
+        int s1 = d1.sal;
+        System.out.println("Variable is accessed in Another Class : "+s1);
+        samePackage sp = new samePackage();
+        sp.samePackageAccess();
+    }
+}
+class samePackage {
+    void samePackageAccess() {
+        AM_Default d2 = new AM_Default();
+        int s2 = d2.sal;
+        System.out.println("Variables are accessed in same Package : "+s2);
+    }
+}
+class childClass extends AM_Default {
+    AM_Default d3 = new AM_Default();
+    //int s4 = d3.age;  NOT Possible
+}
+/*
+OUTPUT:
+Variable is accessed in same Class : 7
+Variable is accessed in Another Class : 7
+Variables are accessed in same Package : 7
+ */
--- src/ex_12_Access_Modifiers_And_Keywords/AM_Protected.java
+++ src/ex_12_Access_Modifiers_And_Keywords/AM_Protected.java
@@ -1,12 +1,13 @@
 package ex_12_Access_Modifiers_And_Keywords;
 
-//Protected variables are accessible in subclasses andin same package
+//Protected variables are accessible in same class, subclasses and in same package
+// and NOT accessible in another class and another Package
 public class AM_Protected {
     public static void main(String[] args) {
 
         AM_Protected_Parent a = new AM_Protected_Parent();
         a.superClassFunction();
-        System.out.println("Variable is accessed in same package : "+a.age);
+        System.out.println("Variable is accessed in another class same package : "+a.age);
         AM_Protected_Child b = new AM_Protected_Child();
         b.sbFunction();
     }
@@ -28,6 +29,6 @@ class AM_Protected_Child extends AM_Protected_Parent {
 /*
 OUTPUT:
 Variable is accessed in same class : 60
-Variable is accessed in same package : 60
+Variable is accessed in another class in same package : 60
 Variable is accessed in subclass : 60
  */
