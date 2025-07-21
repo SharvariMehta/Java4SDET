b794f8ec215f20c8bee2d9ef9350188a6c2e38ea
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-21 23:02:28 +0530

    Updating Encapsulation Code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

--- src/ex_10_Encapsulation/Encapsulation_Demo.java
+++ src/ex_10_Encapsulation/Encapsulation_Demo.java
@@ -4,12 +4,15 @@ public class Encapsulation_Demo {
     public static void main(String[] args) {
         appLogin al = new appLogin();
         al.setUsername("msharvari97");
-        System.out.println(al.getUsername());
-        al.setPassword("Switch@9012");
-        System.out.println(al.getPassword());
+        System.out.println("Username : "+al.getUsername());
+        al.setPassword("Switch@9012", true);
+        // Only admin is allowed to update password in this case
+        System.out.println("Password : "+al.getPassword());
+        al.setPassword("Update@123",false);
+        System.out.println("Msg after update attempt by some else than admin : \n"+al.getPassword());
     }
-
 }
+
 class appLogin {
     public String getUsername() {
         return username;
@@ -22,11 +25,20 @@ class appLogin {
     public String getPassword() {
         return password;
     }
-    public void setPassword(String password) {
-        this.password = password;
+    public void setPassword(String password, boolean isAdmin) {
+        if(isAdmin)
+            this.password = password;
+        else
+            this.password = "You are not allowed to change Password";
     }
     private String password;
 }
-
+/*
+OUTPUT:
+Username : msharvari97
+Password : Switch@9012
+Msg after update attempt by some else than admin :
+You are not allowed to change Password
+ */
 
 
