200150bf9e9fc0e3774dcf2a2f3d6423a57b5f6a
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-22 22:31:19 +0530

    Updating 'this' keyword Code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

---
+++ src/ex_12_Access_Modifiers_And_Keywords/KW_super.java
@@ -0,0 +1,7 @@
+package ex_12_Access_Modifiers_And_Keywords;
+
+public class KW_super {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ex_12_Access_Modifiers_And_Keywords/KW_this.java
@@ -0,0 +1,24 @@
+package ex_12_Access_Modifiers_And_Keywords;
+
+public class KW_this {
+    public static void main(String[] args) {
+        System.out.println("Details of Employee : ");
+        kw_this emp1 = new kw_this(7,1400000);
+        System.out.println("Years of Experience : "+emp1.yearsofex);
+        System.out.println("Salary : "+emp1.salary);
+    }
+    public static class kw_this {
+        int yearsofex;
+        int salary;
+        kw_this(int yearsofex, int salary) {
+            this.yearsofex = yearsofex;
+            this.salary = salary;
+        }
+    }
+}
+/*
+OUTPUT:
+Details of Employee :
+Years of Experience : 7
+Salary : 1400000
+ */
