5b1734ca0665bc50810395a59e4e23a3b97ea356
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-24 23:51:46 +0530

    Updating 'static' keyword code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

---
+++ src/ex_12_Access_Modifiers_And_Keywords/KW_static.java
@@ -0,0 +1,32 @@
+package ex_12_Access_Modifiers_And_Keywords;
+
+public class KW_static {
+    public static void main(String[] args) {
+        System.out.println("Scenario 1 :");
+        eg_static_kw ref1 = new eg_static_kw(10);
+        ref1.displayValue();
+        System.out.println("Scenario 2 :");
+        eg_static_kw ref2 = new eg_static_kw(30);
+        ref2.displayValue();
+    }
+}
+class eg_static_kw {
+    int a = 10;
+    static int b = 20;
+    eg_static_kw(int a) {
+        this.a = a;
+    }
+    void displayValue() {
+        System.out.println("Value of non-static variable a : "+a);
+        System.out.println("Value of static variable b : "+b);
+    }
+}
+/*
+OUTPUT:
+Scenario 1 :
+Value of non-static variable a : 10
+Value of static variable b : 20
+Scenario 2 :
+Value of non-static variable a : 30
+Value of static variable b : 20
+ */
