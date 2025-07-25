9c1c8ecc2f12ca9ccdb9c8375c3491213f88e2ce
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-25 23:53:37 +0530

    Updating 'enum' keyword code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

--- src/ex_12_Access_Modifiers_And_Keywords/KW_enum_QA_ex.java
+++ src/ex_12_Access_Modifiers_And_Keywords/KW_enum_QA_ex.java
@@ -2,7 +2,7 @@ package ex_12_Access_Modifiers_And_Keywords;
 
 public class KW_enum_QA_ex {
     public static void main(String[] args) {
-        System.out.println("URL for vwo Testing : "+urls.vwo.getURL());
+        System.out.println("URL for vwo Testing : "+KW_enum_QA_ex_urls.vwo.getURL());
     }
 }
 /*
--- src/ex_12_Access_Modifiers_And_Keywords/KW_enum_QA_ex_urls.java
+++ src/ex_12_Access_Modifiers_And_Keywords/KW_enum_QA_ex_urls.java
@@ -1,11 +1,11 @@
 package ex_12_Access_Modifiers_And_Keywords;
 
-public enum urls {
+public enum KW_enum_QA_ex_urls {
     vwo("www.vwo.com"),
     katlon("www.katalon.com"),
     google("www.google.com");
     private String url;
-    urls(String url)    {
+    KW_enum_QA_ex_urls (String url)    {
         this.url = url;
     }
     String getURL() {
