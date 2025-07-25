f584cccc0e316e8f62d0df0ad6a90080e99760ea
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-25 23:50:23 +0530

    Updating 'enum' keyword code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

---
+++ src/ex_12_Access_Modifiers_And_Keywords/KW_enum_QA_ex.java
@@ -0,0 +1,11 @@
+package ex_12_Access_Modifiers_And_Keywords;
+
+public class KW_enum_QA_ex {
+    public static void main(String[] args) {
+        System.out.println(urls.vwo.getURL());
+    }
+}
+/*
+OUTPUT:
+www.vwo.com
+ */
---
+++ src/ex_12_Access_Modifiers_And_Keywords/urls.java
@@ -0,0 +1,15 @@
+package ex_12_Access_Modifiers_And_Keywords;
+
+public enum urls {
+    vwo("www.vwo.com"),
+    katlon("www.katalon.com"),
+    google("www.google.com");
+    private String url;
+    urls(String url)    {
+        this.url = url;
+    }
+    String getURL() {
+        return this.url;
+    }
+
+}
