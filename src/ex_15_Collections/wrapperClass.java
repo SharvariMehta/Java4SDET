71ba9e4f813431db362d72a271d2d0780f58a01a
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-26 23:30:51 +0530

    Updating Wrapper Class code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

--- src/ex_15_Collections/wrapperClass.java
+++ src/ex_15_Collections/wrapperClass.java
@@ -2,11 +2,17 @@ package ex_15_Collections;
 
 public class wrapperClass {
     public static void main(String[] args) {
-        Integer i1 = 63;
-        String s1 = i1.toString(); //Converting wrapper class to string
-        System.out.println(s1);
+        int i1 = 10;
+        Integer i2 = i1; //AutoBoxing : JVM will automatically update value from primitive to wrapper
+        System.out.println("AutoBoxing : "+i2);
 
-        int i2 = 32;
+        Integer i3 = 23;
+        int i4 = i3;        // Wrapper is removed, Attribute, Behaviour is lost
+        System.out.println("Unboxing : "+i4);
+
+        Integer i5 = 63;
+        String s1 = i5.toString(); //Converting wrapper class to string
+        System.out.println("Converting wrapper class 'Integer' to string : "+s1);
 
     }
 }
@@ -18,3 +24,9 @@ class wrapperData{
     Float f = 5.7F;
     Boolean b = true;
 }
+/*
+OUTPUT:
+AutoBoxing : 10
+Unboxing : 23
+Converting wrapper class 'Integer' to string : 63
+ */
