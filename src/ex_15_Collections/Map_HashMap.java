7f4087159d668060fba03207d76b73fb5273d3cd
SharvariMehta <sharvarimehta97@gmail.com>
2025-07-30 23:21:49 +0530

    Updating Map Interface code
    Signed-off-by: SharvariMehta <sharvarimehta97@gmail.com>

--- src/ex_15_Collections/Map_HashMap.java
+++ src/ex_15_Collections/Map_HashMap.java
@@ -10,9 +10,17 @@ public class Map_HashMap {
         hm.put(102,"Samrika");
         hm.put(101,"Sharvari");
         System.out.println("HashMap : "+hm);
+        System.out.println("Key Set : "+hm.keySet());
+        System.out.println("Collection of values : "+hm.values());
+        System.out.println("Entry Set : "+hm.entrySet());
+        System.out.println("Is object 1 equal to object 2 ? "+(hm.get(101).equals(hm.get(102))));
     }
 }
 /*
 OUTPUT:
 HashMap : {101=Sharvari, 102=Samrika, 106=Faani}
+Key Set : [101, 102, 106]
+Collection of values : [Sharvari, Samrika, Faani]
+Entry Set : [101=Sharvari, 102=Samrika, 106=Faani]
+Is object 1 equal to object 2 ? false
  */
--- src/ex_15_Collections/Map_LinkedHashMap.java
+++ src/ex_15_Collections/Map_LinkedHashMap.java
@@ -1,18 +1,33 @@
 package ex_15_Collections;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Map_LinkedHashMap {
     public static void main(String[] args) {
-        Map lhm = new LinkedHashMap();
-        lhm.put(106,"Faani");
-        lhm.put(102,"Samrika");
-        lhm.put(101,"Sharvari");
-        System.out.println("LinkedHashMap : "+lhm);
+        Map lhm1 = new LinkedHashMap();
+        lhm1.put(106,"Faani");
+        lhm1.put(102,"Samrika");
+        lhm1.put(101,"Sharvari");
+        System.out.println("LinkedHashMap_1 : "+lhm1);
+
+        Map lhm2 = new LinkedHashMap();
+        lhm2.put(107,"Sam");
+        lhm2.put(104,"Dam");
+        lhm2.put(108,"Jam");
+        System.out.println("LinkedHashMap_2 : "+lhm2);
+
+        ArrayList al = new ArrayList();
+        al.add(lhm1);
+        al.add(lhm2);
+        System.out.println("Map converted to list : "+al);
+
     }
 }
 /*
 OUTPUT:
-LinkedHashMap : {106=Faani, 102=Samrika, 101=Sharvari}
+LinkedHashMap_1 : {106=Faani, 102=Samrika, 101=Sharvari}
+LinkedHashMap_2 : {107=Sam, 104=Dam, 108=Jam}
+Map converted to list : [{106=Faani, 102=Samrika, 101=Sharvari}, {107=Sam, 104=Dam, 108=Jam}]
  */
